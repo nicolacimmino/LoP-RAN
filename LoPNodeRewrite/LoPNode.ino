313784ed6d0ae1bdd03a5a8063550c17ce9bf947
Nicola Cimmino <nicola.cimmino@gmail.com>
2020-07-12 12:50:44 +0200

    Driver rewrite with proper packet splitting

---
+++ LoPNodeRewrite/LoPNode.ino
@@ -0,0 +1,63 @@
+// LopNode implements a LoP-RAN , a low power radio access network.
+// This is just the Arduino module, all the code is in the CPP files
+//  in this way it can be easily compiled on other platforms. This file
+//  provides only the entry points for the Arduino runtime (steup and loop).
+//
+//  Copyright (C) 2020 Nicola Cimmino
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "src/PHY/NRF24L01RadioDriver.h"
+#include "src/EEPROMMap.h"
+
+NRF24L01RadioDriver *radio;
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Setup");
+  byte hwVersion = EEPROM.read(EEPROM_HW_VERSION);
+  Serial.println("creating radio");
+  radio = new NRF24L01RadioDriver(hwVersion);
+  Serial.println("created radio");
+
+  radio->setRXExtendedPreamble(643234);
+  radio->setTXExtendedPreamble(643234);
+  radio->setTXPower(3);
+  radio->setRFChannel(85);
+
+  Serial.println("Setup done");
+}
+
+uint16_t r = 0;
+
+void loop()
+{
+  char buffer[64];
+  memset(buffer, 0, 64);
+  r++;
+  sprintf(buffer, "TEST MESSAGE OF FIXED LENGTH  AND SOME CHANGING ab DATA %i", r);
+  //               1234567890123456789012345678901212345678901234567890123456789012
+  //               0        1         2         3           4         5         6 
+  radio->send(buffer, strlen(buffer));
+  Serial.println("Sent");
+
+  // if (radio->receive(buffer, 64, 100))
+  // {
+  //   Serial.println(buffer);
+  // }
+  // radio->receive(buffer, 32, 100);
+  // Serial.println(buffer);
+}
